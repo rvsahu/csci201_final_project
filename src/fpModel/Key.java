978596850673c31a4b62bcffe7249eb651f768d8
petuh112 <yaop@usc.edu>
2020-11-09 18:22:37 -0800

    key

--- src/fpModel/Key.java
+++ src/fpModel/Key.java
@@ -8,4 +8,8 @@ public class Key extends Item {
 	public Key(String name, String igSpritePath, String invSpritePath) {
 		super(name, igSpritePath, invSpritePath);
 	}
+	
+	public boolean buyable(Player p) {
+		return true;
+	}
 }
