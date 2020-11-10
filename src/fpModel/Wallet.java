7c24da4f77ca5401a091f8556ccc65d20a1af2e1
Rahul S <rvs2019@protonmail.com>
2020-11-09 18:05:25 -0800

    stuff
    stuff

--- src/fpModel/Wallet.java
+++ src/fpModel/Wallet.java
@@ -3,7 +3,7 @@ package fpModel;
 public class Wallet extends ContainerObject{
 
 	@Override
-	public Item takeItem(int itemIndex) {
+	public Item removeItem(int itemIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
