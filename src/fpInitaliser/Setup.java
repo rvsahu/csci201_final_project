5e0bcf120489b02aaf3dc1646ebae36fc6c0711f
Rahul S <rvs2019@protonmail.com>
2020-11-10 19:21:14 -0800

    Game Initial Setup code
    Code needed to setup game model in initial state, graphic elements not done  until later because waiting on some stuff from Zach.

--- src/fpInitaliser/Setup.java
+++ src/fpInitaliser/Setup.java
@@ -1,18 +1,72 @@
 package fpInitaliser;
 
+import fpModel.*;
+
 public class Setup {
 	
 	public static void setupAll() {
-		
+		Room annex = setupAnnex();
+		Room cove = setupCove();
+		//...
+		Doorway ting = new Doorway();
+		annex.addExitRight(ting);
+		cove.addExitLeft(ting);
 	}
 	
-	/**
-	public Room setupAnnex() {
-		//add couch
-		//add couch
-		//add chair
-		//add pc
-		//etc.
+	private static Room setupAnnex() {
+		Room annex = new Room("annex");
+		annex.addToFront(new Podium());
+		//keep adding stuff
+		return annex;
 	}
-	*/
+	
+	private static Room setupMainA() {
+		return null;
+	}
+	
+	private static Room setupMainB() {
+		return null;
+	}
+	private static Room setupMainC() {
+		return null;
+	}
+	private static Room setupMainD() {
+		return null;
+	}
+	
+	private static Room setupStudy1() {
+		return null;
+	}
+	
+	private static Room setupStudy2() {
+		return null;
+	}
+	
+	private static Room setupStudy3() {
+		return null;
+	}
+	
+	private static Room setupStudy4() {
+		return null;
+	}
+	
+	private static Room setupCove() {
+		return null;
+	}
+	
+	private static Room setupLab1() {
+		return null;
+	}
+	
+	private static Room setupHallway() {
+		return null;
+	}
+	
+	private static Room setupMensRoom() {
+		return null;
+	}
+	
+	private static Room setupWomensRoom() {
+		return null;
+	}
 }
