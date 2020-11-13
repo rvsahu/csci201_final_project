dab937904e2658928bef24d55fedac352b6e26b4
Minji Chung <minjichu@usc.edu>
2020-11-13 15:01:06 -0800

    map

---
+++ src/fpModel/Map.java
@@ -0,0 +1,24 @@
+package fpModel;
+
+public class Map {
+	public Map()
+	{
+		
+	}
+	
+	public Room annex;
+	public Room mainA;
+	public Room mainB;
+	public Room mainC;
+	public Room mainD;
+	public Room study1;
+	public Room study2;
+	public Room study3;
+	public Room study4;
+	public Room cove;
+	public Room lab1;
+	public Room hallway1;
+	public Room hallway2;
+	public Room mensRoom;
+	public Room womensRoom;
+}
