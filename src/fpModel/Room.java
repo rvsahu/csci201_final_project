50f30e3797cf723d02898c54e063a8ac8c58d21f
Rahul S <rvs2019@protonmail.com>
2020-11-15 13:41:28 -0800

    Room now supports less than four perspectives
    Room now supports less than four perspectives via the second constructor, but it must have at least one.

--- src/fpModel/Room.java
+++ src/fpModel/Room.java
@@ -17,16 +17,58 @@ public class Room {
 	 */
 	
 	/**
-	 * Constructs an empty room
+	 * Constructs an empty room, with a default of four perspectives. 
 	 * 
 	 * @param name  The name of the room
 	 */
 	public Room(String name) {
-		perspectives[0] = new Perspective(this, name + "_front", Perspective.Direction.FRONT);
-		perspectives[1] = new Perspective(this, name + "_right", Perspective.Direction.RIGHT);
-		perspectives[2] = new Perspective(this, name + "_back", Perspective.Direction.BACK);
-		perspectives[3] = new Perspective(this, name + "_left", Perspective.Direction.LEFT);
+		perspectives[0] = new Perspective(this, name + " Front", Perspective.Direction.FRONT);
+		perspectives[1] = new Perspective(this, name + " Right", Perspective.Direction.RIGHT);
+		perspectives[2] = new Perspective(this, name + " Back", Perspective.Direction.BACK);
+		perspectives[3] = new Perspective(this, name + " Left", Perspective.Direction.LEFT);
 		currentPerspective = 0;
+	}
+	
+	/**
+	 * Constructs an empty room, with perspectives created as specified.
+	 * 
+	 * @param name  The name of the room.
+	 * @param hasFront  Whether the Room has a front view or not.
+	 * @param hasRight  Whether the Room has a right view or not.
+	 * @param hasBack  Whether the Room has a back view or not.
+	 * @param hasLeft  Whether the Room has a left view or not.
+	 */
+	public Room(String name, boolean hasFront, boolean hasRight, boolean hasBack, boolean hasLeft) throws Exception {
+		if (!(hasFront || hasRight || hasBack ||hasLeft)) {
+			throw new Exception(name + " needs to have at least one perspective!");
+		}
+		boolean cdAssigned = false;
+		if (hasFront) {
+			perspectives[0] = new Perspective(this, name + " Front", Perspective.Direction.FRONT);
+			currentPerspective = 0;
+			cdAssigned = true;
+		}
+		if (hasRight) {
+			perspectives[1] = new Perspective(this, name + " Right", Perspective.Direction.RIGHT);
+			if (!(cdAssigned)) {
+				currentPerspective = 1;
+				cdAssigned = true;
+			}
+		}
+		if (hasBack) {
+			perspectives[2] = new Perspective(this, name + " Back", Perspective.Direction.BACK);
+			if (!(cdAssigned)) {
+				currentPerspective = 2;
+				cdAssigned = true;
+			}
+		}
+		if (hasLeft) {
+			perspectives[3] = new Perspective(this, name + " Left", Perspective.Direction.LEFT);
+			if (!(cdAssigned)) {
+				currentPerspective = 3;
+				cdAssigned = true;
+			}
+		}		
 	}
 	
 	/*
@@ -34,7 +76,7 @@ public class Room {
 	 */
 	
 	/**
-	 * Adds an individual object to the currently selected perspective
+	 * Adds a RoomObject to the currently selected perspective. currentPerspective is guaranteed to exist.
 	 * 
 	 * @param rObj  The object to be added.
 	 */
@@ -43,43 +85,51 @@ public class Room {
 	}
 	
 	/**
-	 * Adds an individual object to the front perspective
+	 * Adds a RoomObject to the front perspective. Does nothing if the front perspective doesn't exist.
 	 * 
 	 * @param rObj  The object to be added.
 	 */
 	public void addToFront(RoomObject rObj) {
+		if (perspectives[0] == null)
+			return;
 		perspectives[0].addRoomObject(rObj);
 	}	
 	
 	/**
-	 * Adds an individual object to the right perspective
+	 * Adds a RoomObject to the right perspective. Does nothing if the right perspective doesn't exist.
 	 * 
 	 * @param rObj  The object to be added.
 	 */
 	public void addToRight(RoomObject rObj) {
+		if (perspectives[1] == null)
+			return;
 		perspectives[1].addRoomObject(rObj);
 	}
 	
 	/**
-	 * Adds an individual object to the back perspective
+	 * Adds a RoomObject to the back perspective. Does nothing if the back perspective doesn't exist.
 	 * 
 	 * @param rObj  The object to be added.
 	 */
 	public void addToBack(RoomObject rObj) {
+		if (perspectives[2] == null)
+			return;
 		perspectives[2].addRoomObject(rObj);
 	}
 	
 	/**
-	 * Adds an individual object to the left perspective
+	 * Adds a RoomObject to the left perspective. Does nothing if the left perspective doesn't exist.
 	 * 
 	 * @param rObj  The object to be added.
 	 */
 	public void addToLeft(RoomObject rObj) {
+		if (perspectives[3] == null)
+			return;
 		perspectives[3].addRoomObject(rObj);
 	}
 	
 	/**
-	 * Adds a list of RoomObjects to the currently selected perspective
+	 * Adds a list of RoomObjects to the currently selected perspective. currentPerspective is guaranteed to exist.
 	 * 
 	 * @param rObj  The list of objects to be added.
 	 */
@@ -90,51 +140,59 @@ public class Room {
 	}
 	
 	/**
-	 * Adds a list of RoomObjects to the front perspective
+	 * Adds a list of RoomObjects to the front perspective. Does nothing if the front perspective doesn't exist.
 	 * 
 	 * @param rObj  The list of objects to be added.
 	 */
 	public void addToFront(List<RoomObject> rObjs) {
+		if (perspectives[0] == null)
+			return;
 		for (RoomObject rObj : rObjs) {
 			addToFront(rObj);
 		}
 	}	
 	
 	/**
-	 * Adds a list of RoomObjects to the right perspective
+	 * Adds a list of RoomObjects to the right perspective. Does nothing if the right perspective doesn't exist.
 	 * 
 	 * @param rObj  The list of objects to be added.
 	 */
 	public void addToRight(List<RoomObject> rObjs) {
+		if (perspectives[1] == null)
+			return;
 		for (RoomObject rObj : rObjs) {
 			addToRight(rObj);
 		}
 	}
 	
 	/**
-	 * Adds a list of RoomObjects to the back perspective
+	 * Adds a list of RoomObjects to the back perspective. Does nothing if the back perspective doesn't exist.
 	 * 
 	 * @param rObj  The list of objects to be added.
 	 */
 	public void addToBack(List<RoomObject> rObjs) {
+		if (perspectives[2] == null)
+			return;
 		for (RoomObject rObj : rObjs) {
 			addToBack(rObj);
 		} 
 	}
 	
 	/**
-	 * Adds a list of RoomObjects to the left perspective
+	 * Adds a list of RoomObjects to the left perspective. Does nothing if the left perspective doesn't exist.
 	 * 
 	 * @param rObj  The list of objects to be added.
 	 */
 	public void addToLeft(List<RoomObject> rObjs) {
+		if (perspectives[3] == null)
+			return;
 		for (RoomObject rObj : rObjs) {
 			addToLeft(rObj);
 		}
 	}
 	
 	/**
-	 * Adds a Doorway to the currently selected perspective
+	 * Adds a Doorway to the currently selected perspective. currentPerspective is guaranteed to exist.
 	 * 
 	 * @param exit  The Doorway to be added.
 	 */
@@ -143,7 +201,7 @@ public class Room {
 	}
 	
 	/**
-	 * Adds a Doorway to the currently selected perspective
+	 * Adds a Doorway to the front perspective. Does nothing if the front perspective doesn't exist.
 	 * 
 	 * @param exit  The Doorway to be added.
 	 */
@@ -152,7 +210,7 @@ public class Room {
 	}	
 	
 	/**
-	 * Adds a Doorway to the currently selected perspective
+	 * Adds a Doorway to the right perspective. Does nothing if the right perspective doesn't exist.
 	 * 
 	 * @param exit  The Doorway to be added.
 	 */
@@ -161,7 +219,7 @@ public class Room {
 	}
 	
 	/**
-	 * Adds a Doorway to the currently selected perspective
+	 * Adds a Doorway to the back perspective. Does nothing if the back perspective doesn't exist.
 	 * 
 	 * @param exit  The Doorway to be added.
 	 */
@@ -170,7 +228,7 @@ public class Room {
 	}
 	
 	/**
-	 * Adds a Doorway to the currently selected perspective
+	 * Adds a Doorway to the left perspective. Does nothing if the left perspective doesn't exist.
 	 * 
 	 * @param exit  The Doorway to be added.
 	 */
@@ -209,6 +267,8 @@ public class Room {
 	 */
 	public boolean setPerspective(Perspective perspective) {
 		for (int i = 0; i < 3; i += 1) {
+			if (perspectives[i] == null)
+				continue;
 			boolean isEqual = perspectives[i].equals(perspective);
 			if (isEqual) {
 				currentPerspective = i;
@@ -219,10 +279,26 @@ public class Room {
 	}
 	
 	/**
+	 * Given an index that corresponds to a direction (0 is front, 1 is right, 2 is back, 3 is left), sets the current
+	 * perspective to that index.
+	 * 
+	 * @param pIndex  The index of the perspective.
+	 * @return     True if the perspective exists, false if it doesn't.
+	 */
+	public boolean setPerspective(int pIndex) {
+		if (perspectives[pIndex] == null)
+			return false;
+		currentPerspective = pIndex;
+		return true;
+	}
+	
+	/**
 	 * Setup/deserialisation method. Gives each perspective a reference to this Room as their containing room.
 	 */
 	public void addContainingRooms() {
 		for (int i = 0; i < 4; i += 1) {
+			if (perspectives[i] == null) 
+				continue;
 			perspectives[i].setContainingRoom(this);
 		}
 	}
@@ -310,74 +386,90 @@ public class Room {
 	*/
 	
 	/**
-	 * Sets the layer backgrounds of the front perspective.
+	 * Sets the layer backgrounds of the front perspective. Does nothing if the front perspective doesn't exist.
 	 * 
 	 * @param layerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setLayerBackgroundsFront(String[] layerPaths) {
+		if (perspectives[0] == null)
+			return;
 		perspectives[0].setLayerPaths(layerPaths);
 	}
 	
 	/**
-	 * Sets the layer backgrounds of the right perspective.
+	 * Sets the layer backgrounds of the right perspective. Does nothing if the right perspective doesn't exist.
 	 * 
 	 * @param layerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setLayerBackgroundsRight(String[] layerPaths) {
+		if (perspectives[1] == null)
+			return;
 		perspectives[1].setLayerPaths(layerPaths);
 	}
 	
 	/**
-	 * Sets the layer backgrounds of the back perspective.
+	 * Sets the layer backgrounds of the back perspective. Does nothing if the back perspective doesn't exist.
 	 * 
 	 * @param layerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setLayerBackgroundsBack(String[] layerPaths) {
+		if (perspectives[2] == null)
+			return;
 		perspectives[2].setLayerPaths(layerPaths);
 	}
 	
 	/**
-	 * Sets the layer backgrounds of the left perspective.
+	 * Sets the layer backgrounds of the left perspective. Does nothing if the left perspective doesn't exist.
 	 * 
 	 * @param layerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setLayerBackgroundsLeft(String[] layerPaths) {
+		if (perspectives[3] == null)
+			return;
 		perspectives[3].setLayerPaths(layerPaths);
 	}
 	
 	/**
-	 * Sets the unlit layer backgrounds of the front perspective.
+	 * Sets the unlit layer backgrounds of the front perspective. Does nothing if the front perspective doesn't exist.
 	 * 
 	 * @param unlitLayerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setUnlitLayerBackgroundsFront(String[] unlitLayerPaths) {
+		if (perspectives[0] == null)
+			return;
 		perspectives[0].setUnlitLayerPaths(unlitLayerPaths);
 	}
 	
 	/**
-	 * Sets the unlit layer backgrounds of the right perspective.
+	 * Sets the unlit layer backgrounds of the right perspective. Does nothing if the right perspective doesn't exist.
 	 * 
 	 * @param unlitLayerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setUnlitLayerBackgroundsRight(String[] unlitLayerPaths) {
+		if (perspectives[1] == null)
+			return;
 		perspectives[1].setUnlitLayerPaths(unlitLayerPaths);
 	}
 	
 	/**
-	 * Sets the layer backgrounds of the back perspective.
+	 * Sets the layer backgrounds of the back perspective. Does nothing if the back perspective doesn't exist.
 	 * 
 	 * @param unliLlayerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setUnlitLayerBackgroundsBack(String[] unlitLayerPaths) {
+		if (perspectives[2] == null)
+			return;
 		perspectives[2].setUnlitLayerPaths(unlitLayerPaths);
 	}
 	
 	/**
-	 * Sets the layer backgrounds of the left perspective.
+	 * Sets the layer backgrounds of the left perspective. Does nothing if the left perspective doesn't exist.
 	 * 
 	 * @param unlitLayerPaths  An array of strings containing the file paths to each layer in ascending order.
 	 */
 	public void setUnlitLayerBackgroundsLeft(String[] unlitLayerPaths) {
+		if (perspectives[3] == null)
+			return;
 		perspectives[3].setUnlitLayerPaths(unlitLayerPaths);
 	}
 }
