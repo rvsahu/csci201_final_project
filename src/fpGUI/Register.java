669a9479219028ed32c96d2b97ec0bb6aa224389
petuh112 <yaop@usc.edu>
2020-11-19 15:30:58 -0800

    password check

--- src/fpGUI/Register.java
+++ src/fpGUI/Register.java
@@ -1,5 +1,7 @@
 package fpGUI;
 
+import java.util.HashMap;
+
 import fpGame.GameUtil;
 import fpIO.AutoSaver;
 import javafx.event.ActionEvent;
@@ -28,39 +30,53 @@ public class Register {
         txt1.setText("Username:");
         Text txt2 = new Text();
         txt2.setText("Password:");
+        Text txt3 = new Text();
+        txt3.setText("Confirm Password:");
         
         TextField txtf1 = new TextField();
         TextField txtf2 = new TextField();
+        TextField txtf3 = new TextField();
         
         //TODO add confirm password field
                 
-        txt1.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/3);
+        txt1.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/6);
         txt1.setTranslateX((GameUtil.WINDOW_X* -GameUtil.scalingFactor())*3/40 -50);
         
-        txt2.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/2);
+        txt2.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/3);
         txt2.setTranslateX((GameUtil.WINDOW_X* -GameUtil.scalingFactor())*3/40 -50);
+        
+        txt3.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/2);
+        txt3.setTranslateX((GameUtil.WINDOW_X* -GameUtil.scalingFactor())*3/40 -70);
         
         btn1.setTranslateY((GameUtil.WINDOW_Y* -GameUtil.scalingFactor())/4);
                 
         btn2.setTranslateY((GameUtil.WINDOW_Y* -GameUtil.scalingFactor())/8);
         
-        txtf1.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/3);
+        txtf1.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/6);
         txtf1.setTranslateX((GameUtil.WINDOW_X* GameUtil.scalingFactor())/8);
         txtf1.setPrefWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
         txtf1.setMaxWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
         
-        txtf2.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/2);
+        txtf2.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/3);
         txtf2.setTranslateX((GameUtil.WINDOW_X* GameUtil.scalingFactor())/8);
         txtf2.setPrefWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
         txtf2.setMaxWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
         //txtf2.setMaxWidth(300);
         
+        txtf3.setTranslateY((GameUtil.WINDOW_Y* GameUtil.scalingFactor())/2);
+        txtf3.setTranslateX((GameUtil.WINDOW_X* GameUtil.scalingFactor())/8);
+        txtf3.setPrefWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
+        txtf3.setMaxWidth((GameUtil.WINDOW_X* GameUtil.scalingFactor()*2)/5);
         
+        HashMap<String,String> usermap = new HashMap<String,String>();
+        usermap.put("Mickey", "password1");
+        usermap.put("Hunter", "123123");
         
         EventHandler<ActionEvent> b1 = new EventHandler<ActionEvent>() {
         	@Override public void handle(ActionEvent event) {
         		String userName = txtf1.getText();
         		String passWord = txtf2.getText();
+        		String confirmPassword = txtf3.getText();
         		
         		if(userName.equals("")) {
         			error.setText("No Username!");
@@ -70,13 +86,22 @@ public class Register {
         			error.setText("No Password!");
         			return;
         		}
-        		//TODO add condition for if password and confirm password don't match
         		
-        		//TODO add condition for if username already exists in database
+        		if(usermap.get(userName) != null) {
+        			error.setText("This Username is already taken.");
+        			return;
+        		}
         		
-        		//TODO move to MainMenu with connection to server if/when registration is successful
+        		if(!confirmPassword.equals(passWord)) {
+        			error.setText("The Password does not match the Confirmed Password field.");
+        			return;
+        		}
         		
-        		//if registered successfully call GameUtil.login();
+        		
+        		
+        		
+        		usermap.put(userName, passWord);
+        		GameUtil.login();
         		
         		MainMenu.show(stage, userName, autoSaver); //pass autoSaver here
         	}
@@ -96,13 +121,15 @@ public class Register {
         
         
         StackPane root = new StackPane();
-        root.getChildren().addAll(btn1,btn2,txt1,txt2,txtf1,txtf2,error);
+        root.getChildren().addAll(btn1,btn2,txt1,txt2,txt3,txtf1,txtf2,txtf3,error);
         StackPane.setAlignment(btn1, Pos.BOTTOM_CENTER);
         StackPane.setAlignment(btn2, Pos.BOTTOM_CENTER);
         StackPane.setAlignment(txt1, Pos.TOP_CENTER);
         StackPane.setAlignment(txt2, Pos.TOP_CENTER);
+        StackPane.setAlignment(txt3, Pos.TOP_CENTER);
         StackPane.setAlignment(txtf1, Pos.TOP_CENTER);
         StackPane.setAlignment(txtf2, Pos.TOP_CENTER);
+        StackPane.setAlignment(txtf3, Pos.TOP_CENTER);
         StackPane.setAlignment(error, Pos.TOP_CENTER);
         
         stage.setScene(new Scene(root, GameUtil.WINDOW_X * GameUtil.scalingFactor(), 
